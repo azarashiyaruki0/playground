4d6e75c1dcb0af79fdb3b7db88f318ba42576f28
azarashiyaruki0 <70779667+azarashiyaruki0@users.noreply.github.com>
2020-10-03 17:38:34 +0900

    button active hover

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log('hello world')
+
+function voice1(){
+    console.log(document.getElementById("voice"))
+    target = document.getElementById('voice')
+    target.innerHTML = "HARA HETTA";
+}
+
+function voice2(){    
+    console.log(document.getElementById("voice"))
+    target = document.getElementById('voice')
+    target.innerHTML = "SUSHI TABETAI";
+}
+
+function voice3(){    
+    console.log(document.getElementById("voice"))
+    target = document.getElementById('voice')
+    target.innerHTML = "NIKU TABETAI";
+}
+
+function voice4(){    
+    console.log(document.getElementById("voice"))
+    target = document.getElementById('voice')
+    target.innerHTML = "RAMEN TABETAI";
+}
+
+function voice5(){    
+    console.log(document.getElementById("voice"))
+    target = document.getElementById('voice')
+    target.innerHTML = "HAMBURG TABETAI";
+}
