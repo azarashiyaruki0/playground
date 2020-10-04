fc3f00386bbc0db397bb681a3618bad0511ab530
azarashiyaruki0 <70779667+azarashiyaruki0@users.noreply.github.com>
2020-10-04 09:59:33 +0900

    keyEvent

--- main.js
+++ main.js
@@ -1,31 +1,43 @@
 console.log('hello world')
 
-function voice1(){
-    console.log(document.getElementById("voice"))
-    target = document.getElementById('voice')
-    target.innerHTML = "HARA HETTA";
-}
+'use strict';
 
-function voice2(){    
-    console.log(document.getElementById("voice"))
-    target = document.getElementById('voice')
-    target.innerHTML = "SUSHI TABETAI";
-}
+document.addEventListener('keydown', (event) => {
+    
+    const keyName = event.key;
+    const keyCode = event.code;
+    var el = document.getElementById('voice');
 
-function voice3(){    
-    console.log(document.getElementById("voice"))
-    target = document.getElementById('voice')
-    target.innerHTML = "NIKU TABETAI";
-}
+    if (keyName === 'Control') {
+        // do not alert when only Control key is pressed.
+        return;
+    }
 
-function voice4(){    
-    console.log(document.getElementById("voice"))
-    target = document.getElementById('voice')
-    target.innerHTML = "RAMEN TABETAI";
-}
+    if (event.ctrlKey) {
+        // Even though event.key is not 'Control' (e.g., 'a' is pressed),
+        // event.ctrlKey may be true if Ctrl key is pressed at the same time.
+        el.innerText = `Combination of ctrlKey + ${keyName}`;
+    } else {
+        el.innerText = `Key pressed ${keyName}`;
+    }
 
-function voice5(){    
-    console.log(document.getElementById("voice"))
-    target = document.getElementById('voice')
-    target.innerHTML = "HAMBURG TABETAI";
-}
+    console.log(`keydown ${keyName} ${keyCode}`)
+
+}, false);
+
+document.addEventListener('keyup', (event) => {
+
+    const keyName = event.key;
+    const keyCode = event.code;
+    var time = new date().getTime();
+    var el = document.getElementById('voice');
+  
+    // As the user releases the Ctrl key, the key is no longer active,
+    // so event.ctrlKey is false.
+    if (keyName === 'Control') {
+        el.innerText = 'Control key was released';
+    }
+
+    console.log(`keyup ${keyName} ${keyCode}`)
+
+  }, false);
